924850ed3aa72fffe7dbc1842ed1382896a5c673
Adrianoggm000 <72881543+adrianoggm@users.noreply.github.com>
2025-02-25 13:21:57 +0100

    Se añade la info  medainte el  ValidationPipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common'; //para ñadir la información en logs y feedback de porqué se falla 
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // elimina propiedades que no están definidas en el DTO
+      forbidNonWhitelisted: true, // arroja un error si se envían propiedades extra
+      transform: true, // convierte los datos de entrada al tipo esperado
+    }),
+  )
   await app.listen(process.env.PORT ?? 3000);
+
 }
 bootstrap();
