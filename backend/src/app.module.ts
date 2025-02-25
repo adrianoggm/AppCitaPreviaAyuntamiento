7d2fb3e8447a6ebcd289127eca12f6a82e2c23a6
Adrianoggm000 <72881543+adrianoggm@users.noreply.github.com>
2025-02-25 09:18:10 +0100

    Creo en dominio la clase trámite
    Genero el backend y la clase trámite

---
+++ backend/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { join } from 'path';
+import {ServeStaticModule} from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'frontend'), 
+      exclude: ['/apo/*'] //excluyo las rutas de api
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ backend/src/domain/entities/tramite.entity.ts
@@ -0,0 +1,32 @@
+/*
+    Clase trámite  tiene asociados los siguientes elementos :
+    -id: string         id generado 
+    -nombre: string     nombre del tramite
+    -estado: enum       estado del trámite (pendiente,confirmado, en proceso,finalizado)
+    -fechaInicio: Date    fecha de inicio del trámite 
+    -fechaFin: Date       fecha de finalización del trámite
+    -usuario: string  idusuario que genera y posee el trámite 
+    -codigo: string  codigo generado con referencia al tipo de trá,ite departamiento y num del dia
+    -tipo: TipoTramite  tipo de trámite duda si será necesario o duplicado con nombre.
+    -documentos: vector de Documentos asociados al trámite.
+    -observaciones: string
+*/
+
+export class Tramite {
+    constructor(
+        public readonly id: string,
+        public nombre:string,
+        public estado: string, //CAMBIAR POR ENUM
+        public fechaInicio: Date,
+        public fechaFin: Date,
+        public idusuario: string,
+        public codigo: string,
+        public tipo: string,
+        public tipoTramite: string, //CAMBIAR POR ENUM
+        public documentos: string[],
+        public observaciones: string
+    ){
+        
+
+    }
+}
