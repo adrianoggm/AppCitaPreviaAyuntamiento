83eb0683475511db9e205b98487e9d1e61a7d48d
Adrianoggm000 <72881543+adrianoggm@users.noreply.github.com>
2025-02-27 13:59:03 +0100

    Se puede buscar los trámites por usuario tipo de tramite  y e idtecnico

--- backend/src/domain/interfaces/tramite-repository.interface.ts
+++ backend/src/domain/interfaces/tramite-repository.interface.ts
@@ -4,5 +4,6 @@ import {Tramite} from "../entities/tramite.entity";
 export interface ITramiteRepository {
     // Crea un nuevo trámite pasandole el formato con el departamento . 
     createTramite(tramite: CreateTramiteDto): Promise<Tramite>;
+    findTramites(filtros: { tipoTramite?: string; idusuario?: string; idtecnico?: string } ): Promise<Tramite[]>;
 
 }
--- backend/src/infrastructure/persistence/tramite.repository.ts
+++ backend/src/infrastructure/persistence/tramite.repository.ts
@@ -5,13 +5,28 @@ import { Tramite } from '../../domain/entities/tramite.entity';
 import { ITramiteRepository } from '../../domain/interfaces/tramite-repository.interface';
 import { CreateTramiteDto } from '../../domain/dto/create-tramite.dto';
 
-
 @Injectable()
 export class TramiteRepository implements ITramiteRepository {
     constructor(@InjectModel('Tramite') private readonly tramiteModel: Model<Tramite>) {}
-
+    
     async createTramite(tramite: CreateTramiteDto): Promise<Tramite> {
         const createdTramite = new this.tramiteModel(tramite);
         return createdTramite.save();
     }
+
+    async findTramites(filtros: { tipoTramite?: string; idusuario?: string; idtecnico?: string }): Promise<Tramite[]> {
+        const query: any = {};
+        if (filtros.tipoTramite) {
+          query.tipoTramite = filtros.tipoTramite;
+        }
+        if (filtros.idusuario) {
+          query.idusuario = filtros.idusuario;
+        }
+        if (filtros.idtecnico) {
+          query.idtecnico = filtros.idtecnico;
+        }
+        console.log("Consulta generada:", query);
+        const tramites = await this.tramiteModel.find(query).exec();
+        return tramites.map(doc => doc.toJSON());
+      }
 }
