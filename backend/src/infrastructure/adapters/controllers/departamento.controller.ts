11b1b21ed1b7e7ed807321d7eb4582beac8913a7
Adrianoggm000 <72881543+adrianoggm@users.noreply.github.com>
2025-03-04 09:16:44 +0100

    Fixing all imports from Departamento Module/   Updated TecnicoModule file was writtten as tecnico.model instead of .module
    created departamento.repository and buscar-departamento.use-case

---
+++ backend/src/infrastructure/adapters/controllers/departamento.controller.ts
@@ -0,0 +1,29 @@
+import { Controller,Post,Body,Get,  Query } from "@nestjs/common";
+import { CreateDepartamentoDto } from "src/domain/dto/create-departamento.dto";
+import { Departamento} from "src/domain/entities/departamento.entity";
+import { CreateDepartamentoUseCase } from "src/application/use-cases/create-departamento.use-case"
+import { BuscarDepartamentoUseCase } from "src/application/use-cases/buscar-departamento.use-case"
+
+    //TODO AÑADIR  + RUTAS de usuario para añadir borrar editar buscar algun usuario .
+
+@Controller("api/departamento")
+export class DepartamentoController{
+    constructor (
+        private readonly  createDepartamentoUseCase: CreateDepartamentoUseCase,
+        private readonly  buscarDepartamentoUseCase:  BuscarDepartamentoUseCase
+    ){}
+
+    @Post()
+    async create(@Body() createDepartamentoDto: CreateDepartamentoDto):Promise<Departamento>{
+        return await this.createDepartamentoUseCase.execute(createDepartamentoDto);
+    }
+    //Busco por nombre de usario
+    @Get()
+    async findDepartamentoByName(@Query('nombredepartamento') nombredepartamento:string): Promise <Departamento|null>{
+        return await this.buscarDepartamentoUseCase.execute(nombredepartamento);
+    }
+    /*@Get()   //Busco los que pertenezcan a mismo departamento
+    async findAllUsuarios(@Query("Departamento") nombreusuario:string): Promise <Usuario[]|null>{
+        return await this.buscarUsuarioUseCase.execute(nombreusuario);
+    }*/
+}
