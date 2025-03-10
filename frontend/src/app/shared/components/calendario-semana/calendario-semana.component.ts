622640bbe4d85184905edeb73d91202066e0027c
Adrianoggm000 <72881543+adrianoggm@users.noreply.github.com>
2025-03-10 12:17:09 +0100

    Added calendario-semana component

---
+++ frontend/src/app/shared/components/calendario-semana/calendario-semana.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendario-semana',
+  imports: [],
+  templateUrl: './calendario-semana.component.html',
+  styleUrl: './calendario-semana.component.scss'
+})
+export class CalendarioSemanaComponent {
+
+}
