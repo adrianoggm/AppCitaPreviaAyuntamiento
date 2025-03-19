f2949e0440a6d9057a11040a60b9db564130712e
Adrianoggm000 <72881543+adrianoggm@users.noreply.github.com>
2025-03-19 11:25:32 +0100

    JwT login changed implementing  the logging a jwt session with authenticator
    Created authntication module for creating a session for the  differents api  of the backend.

---
+++ backend/src/infrastructure/authentication/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { UsuarioModule}   from '../adapters/controllers/usuario/usuario.module';
+import { jwtConstants } from './jwtconstants';
+
+@Module({
+  imports: [
+    UsuarioModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
---
+++ backend/src/infrastructure/authentication/jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { jwtConstants } from './jwtconstants';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.secret,
+    });
+  }
+
+  async validate(payload: any) {
+    // Aquí simplemente retornamos la información contenida en el token,
+    // lo cual se adjunta al objeto request.
+    return { userId: payload.sub, nombreusuario: payload.nombreusuario };
+  }
+}
